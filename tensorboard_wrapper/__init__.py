fee9d0c4e22024e86989d8bb6199299a9999bec0
Nathan Gavenski <nathangavenski@gmail.com>
2020-07-30 23:17:29 -0300

    package info

---
+++ tensorboard_wrapper/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tensorboard Wrapper
+
+This is a wrapper class for the Tensorflow's Tensorboard application.
+"""
+
+__version__ = "0.1"
+__author__ = 'Nathan Gavenski'
+__credits__ = 'Machine Learning Theory and Applications Lab"
