7d2f9132c77414b953625af5217e4d1a4e985d75
Nathan Gavenski <nathangavenski@gmail.com>
2023-10-26 17:09:00 +0100

    Fix setup.cfg and __init__

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-import setuptools
+from setuptools import setup
 
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setup()
--- tensorboard_wrapper/__init__.py
+++ tensorboard_wrapper/__init__.py
@@ -4,17 +4,9 @@ Tensorboard Wrapper
 This is a wrapper class for the Tensorflow's Tensorboard application.
 """
 
-from ._version import get_versions
-
 from .tensorboard import Tensorboard
 from .exceptions import BoardAlreadyExistsException
 
 
-__version__ = get_versions()['version']
 __author__ = "Nathan Gavenski"
 __credits__ = "Nathan Gavenski"
-del get_versions
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
